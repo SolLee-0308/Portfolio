e673d84057a66bb978a6b961bb992112113ce2a9
Sol-Lee <march8sollee@gmail.com>
2023-06-21 23:49:29 +0900

    Portfolio

---
+++ Flex_Page01/js/freitag.js
@@ -0,0 +1,43 @@
+var video = $('#freitag_video');
+var btn = $('#freitag_btn');
+
+
+btn.click(function(){
+    if(video.get(0).paused){
+        video.get(0).play();
+        btn.html('<i class="fa fa-pause-circle-o" aria-hidden="true"></i>');
+    }else{
+        video.get(0).pause();                
+        btn.html('<i class="fa fa-circle-circle-o" aria-hidden="true"></i>');
+    }
+})
+
+
+// Slider
+$(function(){
+    $('.slide_gallery').bxSlider({
+        mode: 'horizontal',
+        pager: false,
+        auto: true,
+        autoHover:true,
+        slideWidth: 0,
+        controls: true,
+        autoSlideForOnePage: true,
+        pause: 3500,
+    });
+
+    $('.tab_btn li').on('click',function(){
+        const idx = $(this).index();
+        console.log(idx);
+    
+        $('.tab_btn li').removeClass('on');
+        $(this).addClass('on');
+    
+        $('.freitag_mov .video').hide();
+        $('.freitag_mov .video').eq(idx).stop().show();
+    })
+});
+
+
+
+
