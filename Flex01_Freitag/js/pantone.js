b50c035b7d6d3434bb545997eadd7e4efcbca111
Sol-Lee <march8sollee@gmail.com>
2023-06-28 16:25:49 +0900

    Portfolio

---
+++ Flex01_Freitag/js/pantone.js
@@ -0,0 +1,13 @@
+// Slider
+$(function(){
+    $('.slide_gallery').bxSlider({
+        mode: 'horizontal',
+        pager: true,
+        auto: true,
+        autoHover:true,
+        slideWidth: 0,
+        controls: true,
+        autoSlideForOnePage: true,
+        pause: 3500,
+    });
+});
